a7e74e5ec5226c2dc0fc1f7dbe348fb0693e9e6c
Enzo <kdenzo88@gmail.com>
2024-03-09 18:00:57 +0100

    [ADD] diversity Items in Pokeloot
    Adding temporarily 3 list of items HELD , EVOLUTION , POKEBALL  before adding loot Table

--- src/main/java/fr/lhaven/pokedungeon/block/custom/LootBlock.java
+++ src/main/java/fr/lhaven/pokedungeon/block/custom/LootBlock.java
@@ -4,12 +4,16 @@ import com.cobblemon.mod.common.CobblemonItems;
 import com.cobblemon.mod.common.CobblemonSounds;
 import com.cobblemon.mod.common.api.tags.CobblemonItemTags;
 import com.cobblemon.mod.common.item.PokeBallItem;
+import com.cobblemon.mod.common.item.group.CobblemonItemGroups;
+import fr.lhaven.pokedungeon.PokeDungeon;
 import fr.lhaven.pokedungeon.item.PokeDungeonItems;
+import fr.lhaven.pokedungeon.utils.LootTable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
@@ -29,12 +33,17 @@ public class LootBlock extends Block {
       pPlayer.displayClientMessage(Component.nullToEmpty("You found a loot block!"), true);
         plevel.setBlock(pPos, Blocks.AIR.defaultBlockState(), 3);
         pPlayer.playSound(CobblemonSounds.GUI_CLICK, 1.0F, 1.0F);
+        ItemStack PokeballStack = LootTable.getPokeBalls();
+        ItemStack EvolutionStack = LootTable.getEvolution();
+        ItemStack HeldItemStack = LootTable.getHeldItems();
         ItemStack keystack = new ItemStack(PokeDungeonItems.POKEKEY_ITEM.get(), 1);
-        ItemStack cobblemonStack = new ItemStack(CobblemonItems.DAWN_STONE, 1);
         pPlayer.getInventory().add(keystack);
-        pPlayer.getInventory().add(cobblemonStack);
+        pPlayer.getInventory().add(HeldItemStack);
+        pPlayer.getInventory().add(EvolutionStack);
+        pPlayer.getInventory().add(PokeballStack);
 
-       return InteractionResult.SUCCESS;
+
+        return InteractionResult.SUCCESS;
 
  
     }
---
+++ src/main/java/fr/lhaven/pokedungeon/utils/LootTable.java
@@ -0,0 +1,110 @@
+package fr.lhaven.pokedungeon.utils;
+
+import com.cobblemon.mod.common.CobblemonItems;
+import com.cobblemon.mod.common.api.tags.CobblemonItemTags;
+import com.cobblemon.mod.common.item.group.CobblemonItemGroups;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.openjdk.nashorn.internal.objects.Global.print;
+
+public class LootTable {
+
+    public static final List<ItemStack> EVOLUTION = new ArrayList<>();
+    public  static final List<ItemStack> POKEBALLS = new ArrayList<>();
+    public  static final List<ItemStack> HELD_ITEMS = new ArrayList<>();
+
+
+
+   public LootTable () {
+
+   }
+    public static ItemStack getLoot() {
+        return randomable(EVOLUTION);
+   }
+
+    public static ItemStack getEvolution() {
+        EVOLUTION.add(new ItemStack(CobblemonItems.DAWN_STONE));
+        EVOLUTION.add(new ItemStack(CobblemonItems.DUSK_STONE));
+        EVOLUTION.add(new ItemStack(CobblemonItems.FIRE_STONE));
+        EVOLUTION.add(new ItemStack(CobblemonItems.LEAF_STONE));
+        EVOLUTION.add(new ItemStack(CobblemonItems.MOON_STONE));
+        EVOLUTION.add(new ItemStack(CobblemonItems.SUN_STONE));
+        EVOLUTION.add(new ItemStack(CobblemonItems.THUNDER_STONE));
+        EVOLUTION.add(new ItemStack(CobblemonItems.WATER_STONE));
+        EVOLUTION.add(new ItemStack(CobblemonItems.OVAL_STONE));
+        EVOLUTION.add(new ItemStack(CobblemonItems.SHINY_STONE));
+        return randomable(EVOLUTION);
+
+    }
+
+    public static ItemStack getPokeBalls() {
+        POKEBALLS.add(new ItemStack(CobblemonItems.POKE_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.GREAT_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.ULTRA_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.MASTER_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.DUSK_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.QUICK_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.REPEAT_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.DIVE_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.NEST_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.LUXURY_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.PREMIER_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.DREAM_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.HEAL_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.QUICK_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.SAFARI_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.SPORT_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.FAST_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.LEVEL_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.LURE_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.HEAVY_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.LOVE_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.FRIEND_BALL));
+        POKEBALLS.add(new ItemStack(CobblemonItems.MOON_BALL));
+      return randomable(POKEBALLS);
+    }
+
+    public static ItemStack getHeldItems() {
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.BIG_ROOT));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.BRIGHT_POWDER));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.CHARCOAL));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.DESTINY_KNOT));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.EVERSTONE));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.EXP_SHARE));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.FOCUS_BAND));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.LEFTOVERS));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.LIGHT_CLAY));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.LUCKY_EGG));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.MAGNET));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.METAL_COAT));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.MIRACLE_SEED));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.MYSTIC_WATER));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.NEVER_MELT_ICE));
+        HELD_ITEMS.add(new ItemStack(CobblemonItems.POISON_BARB));
+
+        return randomable(HELD_ITEMS);
+    }
+    private static ItemStack randomable(List<ItemStack> collection) {
+
+
+      int size = collection.size();
+        int index = (int) (Math.random() * size);
+        ItemStack itemStack = collection.get(index);
+        System.out.print("Item: " + itemStack.getDisplayName() + " Index: " + index + " Size: " + size);
+        System.out.print("Item: " + itemStack.getDisplayName() + " Index: " + index + " Size: " + size);
+        System.out.print("Item: " + itemStack.getDisplayName() + " Index: " + index + " Size: " + size);
+        System.out.print("Item: " + itemStack.getDisplayName() + " Index: " + index + " Size: " + size);
+        System.out.print("Item: " + itemStack.getDisplayName() + " Index: " + index + " Size: " + size);
+        System.out.print("Item: " + itemStack.getDisplayName() + " Index: " + index + " Size: " + size);
+        System.out.print("Item: " + itemStack.getDisplayName() + " Index: " + index + " Size: " + size);
+
+
+        return itemStack;
+    }
+}
+
+
