0eb99279a6e1ed7819a3e662714c70ec51b4530a
Enzo <kdenzo88@gmail.com>
2024-03-10 11:49:30 +0100

    removing LootBlock use

--- src/main/java/fr/lhaven/pokedungeon/block/custom/LootBlock.java
+++ src/main/java/fr/lhaven/pokedungeon/block/custom/LootBlock.java
@@ -1,20 +1,7 @@
 package fr.lhaven.pokedungeon.block.custom;
 
-import com.cobblemon.mod.common.CobblemonSounds;
-import fr.lhaven.pokedungeon.item.PokeDungeonItems;
-import fr.lhaven.pokedungeon.utils.LootTable;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 
 public class LootBlock extends Block {
 
@@ -22,31 +9,6 @@ public class LootBlock extends Block {
         super(pProperties);
 
     }
-
-    public InteractionResult use(BlockState pState , Level plevel , BlockPos pPos , Player pPlayer , InteractionHand pHand , BlockHitResult pHit){ {
-
-   //on click of the block
-      pPlayer.displayClientMessage(Component.nullToEmpty("You found a loot block!"), true);
-      plevel.setBlock(pPos, Blocks.AIR.defaultBlockState(), 3);
-      pPlayer.playSound(CobblemonSounds.GUI_CLICK, 1.0F, 1.0F);
-
-        plevel.addParticle(ParticleTypes.WAX_OFF , pPos.getX(), pPos.getY()+1, pPos.getZ(), 0.5D, 0.5D, 0.5D);
-// Loot
-        ItemStack PokeballStack = LootTable.getPokeBalls();
-        ItemStack EvolutionStack = LootTable.getEvolution();
-        ItemStack HeldItemStack = LootTable.getHeldItems();
-        ItemStack keystack = new ItemStack(PokeDungeonItems.POKEKEY_ITEM.get(), 1);
-        pPlayer.getInventory().add(keystack);
-        pPlayer.getInventory().add(HeldItemStack);
-        pPlayer.getInventory().add(EvolutionStack);
-        pPlayer.getInventory().add(PokeballStack);
-        };
-
-        return InteractionResult.SUCCESS;
-
- 
-    }
-
 }
 
 
--- src/main/java/fr/lhaven/pokedungeon/block/entity/LootBall.java
+++ src/main/java/fr/lhaven/pokedungeon/block/entity/LootBall.java
@@ -4,35 +4,38 @@ import com.cobblemon.mod.common.CobblemonSounds;
 import fr.lhaven.pokedungeon.item.PokeDungeonItems;
 import fr.lhaven.pokedungeon.utils.LootTable;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 
-public class LootBall extends BaseEntityBlock  {
+public class LootBall extends BaseEntityBlock {
 
 public static final VoxelShape SHAPE = Block.box(6, 0, 6, 11, 5, 11);
+public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
 
 public LootBall(Properties pProperties) {
         super(pProperties);
-    }
+        pProperties.lightLevel((pState) -> 2);
 
+}
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE;
@@ -47,7 +50,7 @@ public LootBall(Properties pProperties) {
 
         //on click of the block
         pPlayer.displayClientMessage(Component.nullToEmpty("You found a loot block!"), true);
-        plevel.setBlock(pPos, Blocks.AIR.defaultBlockState(), 3);
+        plevel.removeBlock(pPos, false);
         pPlayer.playSound(CobblemonSounds.GUI_CLICK, 1.0F, 1.0F);
 
         plevel.addParticle(ParticleTypes.WAX_OFF , pPos.getX()+0.5, pPos.getY()+0.5, pPos.getZ()+0.5, 0.5D, 0.5D, 0.5D);
@@ -64,10 +67,7 @@ public LootBall(Properties pProperties) {
     };
 
         return InteractionResult.SUCCESS;
-
-
     }
-
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
