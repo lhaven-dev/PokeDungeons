771e489fa9ce31113d8f5a0cab52a0fce7491d8c
Enzo <kdenzo88@gmail.com>
2024-04-01 16:29:03 +0200

    First Implement Spawn by keys

--- src/main/java/fr/lhaven/pokedungeon/block/custom/LootBlock.java
+++ src/main/java/fr/lhaven/pokedungeon/block/custom/LootBlock.java
@@ -2,15 +2,23 @@ package fr.lhaven.pokedungeon.block.custom;
 
 
 import com.cobblemon.mod.common.Cobblemon;
+import com.cobblemon.mod.common.CobblemonEntities;
+import com.cobblemon.mod.common.api.pokemon.PokemonSpecies;
 import com.cobblemon.mod.common.api.spawning.CobblemonSpawnPools;
 import com.cobblemon.mod.common.api.spawning.CobblemonSpawningProspector;
 import com.cobblemon.mod.common.api.spawning.SpawnBucket;
 import com.cobblemon.mod.common.api.spawning.SpawnLoader;
 import com.cobblemon.mod.common.api.spawning.context.SpawningContext;
 import com.cobblemon.mod.common.api.spawning.spawner.Spawner;
+import com.cobblemon.mod.common.battles.BattleBuilder;
+import com.cobblemon.mod.common.battles.BattleFormat;
+import com.cobblemon.mod.common.command.GiveAllPokemon;
 import com.cobblemon.mod.common.command.GivePokemon;
 import com.cobblemon.mod.common.command.SpawnAllPokemon;
 import com.cobblemon.mod.common.command.SpawnPokemon;
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
+import com.cobblemon.mod.common.pokemon.Pokemon;
+import com.mojang.brigadier.Command;
 import fr.lhaven.pokedungeon.PokeDungeon;
 import fr.lhaven.pokedungeon.item.PokeDungeonItems;
 import net.minecraft.core.BlockPos;
@@ -25,6 +33,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.codehaus.plexus.util.cli.shell.CommandShell;
 
 public class LootBlock extends Block {
 
@@ -42,8 +51,13 @@ public class LootBlock extends Block {
      if( pPlayer.getMainHandItem().is(PokeDungeonItems.POKEKEY_ITEM.get())){
          pPlayer.getMainHandItem().shrink(1);
 
-         pPlayer.teleportTo(0, 100, 0);
-    }
+         Pokemon ditto = new Pokemon();
+         ditto.setSpecies(PokemonSpecies.INSTANCE.getByPokedexNumber(132, Cobblemon.MODID));
+         PokemonEntity dittoEntity = new PokemonEntity( pLevel, ditto , CobblemonEntities.POKEMON);
+         dittoEntity.setPokemon(ditto);
+         dittoEntity.setPos(0, 100, 0);
+         pLevel.addFreshEntity(dittoEntity);
+     }
         return  InteractionResult.SUCCESS;
 }
 }
